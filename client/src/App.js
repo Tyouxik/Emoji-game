ca3b8d38bc709737187d5121da63ba88762c41ac
Tyouxik <paulinemarie.didier@gmail.com>
2020-08-18 16:08:18 +0200

    create globalstyle.js and set up in index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
 import Game from "./Components/Game/Game";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/game" component={Game} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/game" component={Game} />
+      </Switch>
+    </>
   );
 }
 
---
+++ client/src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+// @import url("https://fonts.googleapis.com/css2?family=Overpass&display=swap")
+export const GlobalStyle = createGlobalStyle`
+
+* {
+    margin:0;
+    padding:0;
+    font-family: 'Overpass';
+
+}
+
+body, html {
+    border: 1px solid black;
+    
+} 
+
+h1 {
+    font-family: 'Overpass';
+}
+`;
