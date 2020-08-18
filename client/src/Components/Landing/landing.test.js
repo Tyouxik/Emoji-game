ce14f18be58e85d6ff318c5c546c1d7c048b7825
Tyouxik <paulinemarie.didier@gmail.com>
2020-08-18 22:28:30 +0200

    test landing component style with snapshot

--- client/src/Components/Landing/landing.test.js
+++ client/src/Components/Landing/landing.test.js
@@ -2,14 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import renderer from "react-test-renderer";
 import "jest-styled-components";
+import { BrowserRouter as Router } from 'react-router-dom';
 import { LandingWrapper, LandingLink } from "./style-landing";
 
-test("it works", () => {
-  const tree = renderer.create(<LandingWrapper />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
 
-test("it works", () => {
-  const tree = renderer.create(<LandingLink />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe('check the style of Landing component', () => {
+
+  test("check style of LandingWrapper", () => {
+    const tree = renderer.create(<LandingWrapper />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("check style of LandingLink to Game", () => {
+    const tree = renderer.create(<Router><LandingLink to="/game" /></Router>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("check style of LandingLink to Instructions", () => {
+    const tree = renderer.create(<Router><LandingLink to="/instructions" /></Router>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+})
+
+
+
