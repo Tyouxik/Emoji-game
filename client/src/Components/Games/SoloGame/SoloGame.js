e68899da7ef42ddf3a42384fcd7f7bd903fac3a2
Tyouxik <paulinemarie.didier@gmail.com>
2020-08-31 16:34:31 +0200

    create Timer component and create useeffect to shuffle deck and pick up card for board

---
+++ client/src/Components/Games/SoloGame/SoloGame.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { Deck } from "../../../Game Logic/deck";
+import Timer from "../Sub-components/Timer";
+import Board from "../Sub-components/Board";
+import { shuffleCards, pick } from "../../../Game Logic/game";
+
+export default function SoloGame() {
+  // const [selectedCards, setSelectedCards] = useState([]);
+  // //     selectedIndex = [];
+  // //     discartedCards = [];
+  // const [foundSets, setFoundSets] = useState(0);
+  const [boardCards, setBoardCards] = useState([]);
+  const [timeIsUp, setTimeIsUp] = useState(false);
+  const [deck, setDeck] = useState(Deck);
+
+  // Shuffle cards at the beginning of the game
+  useEffect(() => {
+    // console.log("I am shuffling");
+    setDeck((deck) => {
+      shuffleCards(deck);
+    });
+  }, []);
+
+  //Add 12 cards to the board at the beginning of the game
+  useEffect(() => {
+    // console.log("I am pickingup cards");
+    // setBoardCards((boardCards) => {
+    pick(12, boardCards, deck);
+
+    // });
+  }, []);
+
+  if (timeIsUp) {
+    return <h1>Time is up</h1>; //score component
+  } else {
+    return (
+      <>
+        <Timer maxMins={1} setTimeIsUp={setTimeIsUp} />
+        <div>
+          <h1 id="title">Game On</h1>
+        </div>
+        <Board boardCards={boardCards} />
+      </>
+    );
+  }
+}
---
+++ client/src/Components/Games/Sub-components/Timer.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+export default function Timer(props) {
+  const { maxMins, setTimeIsUp } = props;
+  const [seconds, setSeconds] = useState(maxMins * 60);
+
+  function formatTime(timeInSec) {
+    let secs = timeInSec % 60;
+    secs = secs >= 9 ? `${secs}` : `0${secs}`;
+    let mins = Math.floor(timeInSec / 60) % 60;
+    mins = mins >= 9 ? `${mins}` : `0${mins}`;
+    return `${mins}:${secs}`;
+  }
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds((seconds) => seconds - 1);
+      } else {
+        clearInterval(interval);
+        setTimeIsUp(true);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [seconds]);
+
+  return <h3>Timer: {formatTime(seconds)}</h3>;
+}
