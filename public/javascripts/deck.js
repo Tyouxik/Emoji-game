93dd1a49fe735cbe330b4575e2f216677ac4e529
Tyouxik <paulinemarie.didier@gmail.com>
2020-10-01 12:51:59 +0200

    refactor: :truck: Move the game logic file to the backend

--- public/javascripts/deck.js
+++ public/javascripts/deck.js
@@ -3,7 +3,7 @@ const colors = ["yellow", "blue", "red"];
 const numbers = [1, 2, 3];
 const shadows = ["left", "right", "none"];
 
-function getDeck() {
+function createDeck() {
   let deck = [];
   for (let i = 0; i < types.length; i++) {
     for (let j = 0; j < colors.length; j++) {
@@ -14,17 +14,37 @@ function getDeck() {
             color: colors[j],
             number: numbers[k],
             shadow: shadows[l],
-            image: `${types[i]}-${colors[j]}-${numbers[k]}-${shadows[l]}.jpg`,
+            imagePath: `${types[i]}-${colors[j]}-${numbers[k]}-${shadows[l]}.jpg`,
           };
           deck.push(card);
         }
       }
     }
   }
-  for (let m = 0; m < deck.length; m++) {
-    deck[m].id = m.toString();
-  }
+  // for (let m = 0; m < deck.length; m++) {
+  //   deck[m].id = m.toString();
+  // }
   return deck;
 }
+let deck = createDeck();
 
-export const Deck = getDeck();
+const getScore = (set) => {
+  let score = {
+    number: { 1: 1, 2: 2, 3: 3 },
+    type: { cool: 10, laugh: 20, hug: 30 },
+    color: { red: 100, yellow: 200, blue: 300 },
+    shadow: { none: 1000, left: 2000, right: 3000 },
+  };
+  // let totalScore = 0;
+  return set.map((card) => {
+    let cardScore = 0;
+    for (let feature in score) {
+      cardScore += score[feature][card[feature]];
+    }
+    card.score = cardScore;
+    return card;
+  });
+};
+deck = getScore(deck);
+console.log(deck);
+export { deck };
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,125 +1,23 @@
-//Helper functions
-
-//Find the possible combos of three different arrays
-const allDiffCombo = (arr1, arr2, arr3) => {
-  let allCombo = [];
-  //console.log(arr1)
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      for (let k = 0; k < arr3.length; k++) {
-        allCombo.push([arr1[i], arr2[j], arr3[k]]);
-      }
-    }
-  }
-  return allCombo;
-};
-
-//Find the possible combos of one array
-const allCombo = (cards) => {
-  let results = [];
-  if (cards.length === 0) {
-    return null;
-  }
-  for (let i = 0; i < cards.length - 1; i++) {
-    for (let j = i + 1; j < cards.length; j++) {
-      for (let k = i + 2; k < cards.length; k++) {
-        results.push([cards[i], cards[j], cards[k]]);
-      }
-    }
-  }
-  return results;
-};
-//Check the sets based on scores
-
-//Generate all the scores for good sets
-function goodSetScores() {
-  let numberScore = [3, 6, 9];
-  let typeScore = [30, 60, 90];
-  let colorScore = [300, 600, 900];
-  let shadowScore = [3000, 6000, 9000];
-  let goodScores = [];
-  for (let number of numberScore) {
-    for (let type of typeScore) {
-      for (let color of colorScore) {
-        for (let shadow of shadowScore) {
-          goodScores.push(number + type + color + shadow);
-        }
-      }
-    }
-  }
-  return goodScores;
-}
-const goodScores = goodSetScores();
-
-//Calculate score for an array of 3 cards
-let score = {
-  number: { 1: 1, 2: 2, 3: 3 },
-  type: { cool: 10, laugh: 20, hug: 30 },
-  color: { red: 100, yellow: 200, blue: 300 },
-  shadow: { none: 1000, left: 2000, right: 3000 },
-};
-const getScore = (set) => {
-  let totalScore = 0;
-  set.forEach((card) => {
-    for (let feature in score) {
-      totalScore += score[feature][card[feature]];
-    }
-  });
-  return totalScore;
-};
-
-//Check if 3 cards are a set
-const checkIfSet = (set) => goodScores.includes(getScore(set));
-
-const checkIfSetInBoard = (set) => {
-  let yellowCards = set.filter((card) => card.color === "yellow");
-  let redCards = set.filter((card) => card.color === "red");
-  let blueCards = set.filter((card) => card.color === "blue");
-  let goodSet = [];
-  // Find all same color sets
-  allCombo(yellowCards).forEach((set) => {
-    if (checkIfSet(set)) {
-      goodSet.push(set);
-    }
-  });
-  allCombo(redCards).forEach((set) => {
-    if (checkIfSet(set)) {
-      goodSet.push(set);
-    }
-  });
-  allCombo(blueCards).forEach((set) => {
-    if (checkIfSet(set)) {
-      goodSet.push(set);
-    }
-  });
-  const allDiffColor = allDiffCombo(yellowCards, redCards, blueCards);
-  allDiffColor.forEach((set) => {
-    if (checkIfSet(set)) {
-      goodSet.push(set);
-    }
-  });
-  return goodSet;
-};
-
 // Shuffle cards
 
-function shuffleCards(cards) {
+function shuffleCards(cardArray) {
   for (let i = 0; i < 1400; i++) {
-    let location1 = Math.floor(Math.random() * cards.length);
-    let location2 = Math.floor(Math.random() * cards.length);
-    let tmp = cards[location1];
-    cards[location1] = cards[location2];
-    cards[location2] = tmp;
+    let location1 = Math.floor(Math.random() * cardArray.length);
+    let location2 = Math.floor(Math.random() * cardArray.length);
+    let tmp = cardArray[location1];
+    cardArray[location1] = cardArray[location2];
+    cardArray[location2] = tmp;
   }
-  return cards;
+  return cardArray;
 }
+
 // Pick cards from deck to board
 function removeCard(deck, num) {
   return deck.slice(0, deck.length - num);
 }
-function addCard(deck, num, boardCardsState) {
+function addCard(deck, num, boardCards) {
   let addedCards = deck.slice(deck.length - num);
-  return [...(boardCardsState || []), ...addedCards];
+  return [...(boardCards || []), ...addedCards];
 }
 
 //Select a card
@@ -154,18 +52,7 @@ export {
   shuffleCards,
   removeCard,
   addCard,
-  checkIfSet,
   changeSelectedCards,
   addSet,
   removeSet,
-  goodScores,
-  getScore,
-  checkIfSetInBoard,
 };
-
-// all possible combinations
-// let zeroCards= [];
-
-// let cards = ["card1", "card2","card3", "card4","card5","card6", "card7","card8","card9","card10","card11","card12"]
-
-// allCombinations(cards)
