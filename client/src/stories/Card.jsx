376df699b9af48912d2127c85c2ab0ad8833d789
Tyouxik <paulinemarie.didier@gmail.com>
2022-02-20 18:27:38 +0100

    format Card component and story

--- client/src/stories/Card.jsx
+++ client/src/stories/Card.jsx
@@ -1,22 +1,35 @@
-import React from "react";
-import classnames from "classnames";
-import './Card.css'
+/* eslint-disable require-jsdoc */
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import './Card.css';
 
 export default function Card(props) {
-  const { card, selectCard, isSelected, showHint, isHint } = props;
+  const {card, selectCard, isSelected, showHint, isHint} = props;
 
-  const cardStyle = classnames('card',{
-      'selected': isSelected,
-      'hint': showHint && isHint,
+  const cardStyle = classnames('card', {
+    'selected': isSelected,
+    'hint': showHint && isHint,
   });
 
   const imgStyle = classnames('card-image');
 
-  
 
   return (
-      <div className={cardStyle} /* onClick={() => selectCard(card.id)} */>
-          <img className={imgStyle} src={`images/${card.imagePath}`} alt={card.image} />
-      </div>
+    <div className={cardStyle} onClick={() => selectCard(card.id)}>
+      <img
+        className={imgStyle}
+        src={`images/${card.imagePath}`}
+        alt={card.image} />
+    </div>
   );
 }
+
+Card.propTypes = {
+  card: PropTypes.array.required,
+  selectCard: PropTypes.func.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  showHint: PropTypes.bool.isRequired,
+  isHint: PropTypes.bool.isRequired,
+};
+
--- client/src/stories/Card.stories.jsx
+++ client/src/stories/Card.stories.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 
-import Card from "./Card";
+import Card from './Card';
 
 export default {
-    title: 'Card',
-    component: Card,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-      card: { required:true },
-      isSelected: {control: 'boolean', required:true}, 
-      showHint: {control: 'boolean', required:true}, 
-      isHint: {control: 'boolean', required:true}
-    },
-    args: {
-        card: {id:"random id", imagePath:`cool-blue-2-right.jpg`},
-        isSelected: true,
-        showHint: false,
-        isHint:false
-    }
-  };
+  title: 'Card',
+  component: Card,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    card: {required: true},
+    isSelected: {control: 'boolean', required: true},
+    showHint: {control: 'boolean', required: true},
+    isHint: {control: 'boolean', required: true},
+  },
+  args: {
+    card: {id: 'random id', imagePath: `cool-blue-2-right.jpg`},
+    isSelected: false,
+    showHint: false,
+    isHint: false,
+  },
+};
 
-  const Template = (args) => <Card {...args} />;
+const Template = (args) => <Card {...args} />;
 
-  export const Regular = Template.bind({});
-  
-  
-  export const Selected = Template.bind({});
-  Selected.args= {
-    isSelected: true,
-  };
+export const Regular = Template.bind({});
+
+export const Selected = Template.bind({});
+Selected.args= {
+  isSelected: true,
+};
