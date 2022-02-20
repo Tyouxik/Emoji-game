e2261dfa01b39dc869fc92f6990705f6537d8510
Tyouxik <paulinemarie.didier@gmail.com>
2022-02-20 22:21:14 +0100

    create image resizing script

---
+++ client/public/resizeImages.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const sharp = require('sharp');
+
+fs.readdir('images', function(err, files) {
+  // handling error
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+  // listing all files using forEach
+  files.forEach(async function(file) {
+    // Do whatever you want to do with the file
+    try {
+      await sharp(`images/${file}`)
+          .resize({
+            width: 300,
+            height: 200,
+          })
+          .toFile(`images-sm/${file}`);
+
+      console.log('Successfully resized an image!');
+    } catch (err) {
+      console.log('Sharp error', {file, err});
+    }
+  });
+});
+
+
