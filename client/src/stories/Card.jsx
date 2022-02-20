4d31351a527cf83de935a331137117101761c83c
Tyouxik <paulinemarie.didier@gmail.com>
2022-02-20 11:53:24 +0100

    design basic card

--- client/src/stories/Card.jsx
+++ client/src/stories/Card.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import { CardImg } from "./Card-style.js";
+import classnames from "classnames";
+import './Card.css'
 
 export default function Card(props) {
   const { card, selectCard, isSelected, showHint, isHint } = props;
-  let divStyle = classnames({
+
+  const cardStyle = classnames('card',{
       'selected': isSelected,
       'hint': showHint && isHint,
-  })
+  });
+
+  const imgStyle = classnames('card-image');
+
+  
+
   return (
-      <div className={divStyle}>
-          <img src={`images/${card.imagePath}`} alt={card.image} />
+      <div className={cardStyle} /* onClick={() => selectCard(card.id)} */>
+          <img className={imgStyle} src={`images/${card.imagePath}`} alt={card.image} />
       </div>
   );
 }
-{/* <CardImg
-  showHint={showHint}
-  isHint={isHint}
-  isSelected={isSelected}
-  onClick={() => selectCard(card.id)}
-  id={card.id}
-  src={`images/${card.imagePath}`}
-  alt={card.image}
-/> */}
