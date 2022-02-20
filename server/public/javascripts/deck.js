d8a22e0bfe094e94d87bc2ca09e9d42e72bb6110
Tyouxik <paulinemarie.didier@gmail.com>
2022-02-20 18:25:51 +0100

    change deck structure

--- server/public/javascripts/deck.js
+++ server/public/javascripts/deck.js
@@ -1,18 +1,17 @@
 //@ts-check
 
-const types = ["cool", "hug", "laugh"];
+const types = ["cool", "hug", "rolling"];
 const colors = ["yellow", "blue", "red"];
 const numbers = [1, 2, 3];
-const shadows = ["left", "right", "none"];
+const tilted = ["left", "right", "none"];
 
 export const addScoreToCards = (set) => {
   let score = {
     number: { 1: 1, 2: 2, 3: 3 },
-    type: { cool: 10, laugh: 20, hug: 30 },
+    type: { cool: 10, rolling: 20, hug: 30 },
     color: { red: 100, yellow: 200, blue: 300 },
-    shadow: { none: 1000, left: 2000, right: 3000 },
+    tilted: { none: 1000, left: 2000, right: 3000 },
   };
-  // let totalScore = 0;
   return set.map((card) => {
     let cardScore = 0;
     for (let feature in score) {
@@ -28,13 +27,13 @@ export function createDeck() {
   for (let i = 0; i < types.length; i++) {
     for (let j = 0; j < colors.length; j++) {
       for (let k = 0; k < numbers.length; k++) {
-        for (let l = 0; l < shadows.length; l++) {
+        for (let l = 0; l < tilted.length; l++) {
           let card = {
             type: types[i],
             color: colors[j],
             number: numbers[k],
-            shadow: shadows[l],
-            imagePath: `${types[i]}-${colors[j]}-${numbers[k]}-${shadows[l]}.jpg`,
+            tilted: tilted[l],
+            imgName: `${types[i]}-${colors[j]}-${numbers[k]}-${tilted[l]}.jpg`,
           };
           deck.push(card);
         }
