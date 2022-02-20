3f2fd13eeafc6907b0ad4d451b13cd8c7a1835b8
Tyouxik <paulinemarie.didier@gmail.com>
2022-02-20 18:26:40 +0100

    format button.jsx

--- client/src/stories/Button.jsx
+++ client/src/stories/Button.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable valid-jsdoc */
 import React from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
@@ -5,13 +6,16 @@ import './button.css';
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({primary, backgroundColor, size, label, ...props}) => {
+  const mode = primary ? 'storybook-button--primary' :
+    'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
+      className={
+        ['storybook-button', `storybook-button--${size}`, mode].join(' ')
+      }
+      style={backgroundColor && {backgroundColor}}
       {...props}
     >
       {label}
